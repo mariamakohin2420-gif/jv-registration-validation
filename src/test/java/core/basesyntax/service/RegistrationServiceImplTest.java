d08299de74eebdb050d4966fe05652bf92cbfeb1
Mariia Makohin <maria.makohin2420@gmail.com>
2026-08-18 16:16:22 +0300

    my-tests

---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,114 @@
+package core.basesyntax.service;
+import core.basesyntax.exception.RegistrationException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class RegistrationServiceImplTest {
+    private RegistrationService registrationService;
+
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+    @Test
+    void register_validUser_ok() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setPassword("validPassword");
+        user.setAge(20);
+
+        User registeredUser = registrationService.register(user);
+        assertNotNull(registeredUser);
+        assertEquals(user.getLogin(), registeredUser.getLogin());
+    }
+    @Test
+    void register_nullUser_notOk() {
+        assertThrows(RegistrationException.class, () -> registrationService.register(null));
+    }
+    @Test
+    void register_nullLogin_notOk() {
+        User user = createValidUser();
+        user.setLogin(null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+    @Test
+    void register_shortLogin_notOk() {
+        User user = createValidUser();
+        user.setLogin("abcd");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+    @Test
+    void register_minValidLoginLength_ok() {
+        User user = createValidUser();
+        user.setLogin("123456");
+        User registeredUser = registrationService.register(user);
+        assertNotNull(registeredUser);
+    }
+    @Test
+    void register_exsistingLogin_notOk() {
+        User  firstUser = createValidUser();
+        firstUser.setLogin("existingLogin");
+        registrationService.register(firstUser);
+        User secondUser = createValidUser();
+        secondUser.setLogin("existingLogin");
+        assertThrows(RegistrationException.class, () -> registrationService.register(secondUser));
+    }
+    @Test
+    void register_nullPassword_notOk() {
+        User user = createValidUser();
+        user.setPassword(null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+    @Test
+    void register_shortPassword_notOk() {
+        User user = createValidUser();
+        user.setPassword("12345");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+    @Test
+    void regiter_minValidPasswordLength_ok() {
+        User user = createValidUser();
+        user.setPassword("123456");
+        User registeredUser = registrationService.register(user);
+        assertNotNull(registeredUser);
+    }
+    @Test
+    void register_nullAge_notOk() {
+        User user = createValidUser();
+        user.setAge(null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+    @Test
+    void register_underAgeUser_notOk() {
+        User user = createValidUser();
+        user.setAge(17);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+    @Test
+    void register_negativeAge_notOk() {
+        User user = createValidUser();
+        user.setAge(-1);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+    @Test
+    void register_minValidAge_ok() {
+        User user = createValidUser();
+        user.setAge(18);
+        User registeredUser = registrationService.register(user);
+        assertNotNull(registeredUser);
+    }
+    private User createValidUser() {
+        User user = new User();
+        user.setLogin("defaultLogin");
+        user.setPassword("defaultPassword");
+        user.setAge(25);
+        return user;
+    }
+
+}
